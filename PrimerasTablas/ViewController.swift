95a91c86f8f6823f6a366711458a811ea52d034f
DAM <sergiodiaz808@gmail.com>
2016-12-13 20:55:49 +0100

    Tablas de multiplicar.

--- PrimerasTablas/ViewController.swift
+++ PrimerasTablas/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var numero = 0;
+    
+    @IBOutlet weak var valor: UISlider!
+    @IBOutlet weak var tabla: UITableView!
+    
+    @IBAction func slideBar(sender: AnyObject) {
+        //Cogemos el valor del slidebar
+        numero = Int(valor.value)
+        //Recargamos la tabla
+        tabla.reloadData()
     }
+    
+    override func viewDidLoad() {super.viewDidLoad()}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func didReceiveMemoryWarning() {super.didReceiveMemoryWarning()}
+    
+    //Numero de celdas
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 10
     }
-
+    
+    //Valor de las celdas
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let micelda = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "celda")
+        micelda.textLabel?.text = "\(numero) X \(indexPath.row) = \(numero * indexPath.row)"
+        return micelda
+    }
 
 }
 
